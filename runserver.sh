eed0556ed8f421b9e0af39ff239eb789854e20a5
KiTech <emmakagbabuwe@gmail.com>
2023-06-19 22:07:33 +0000

    initial commit

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #set compute region/zone
-gcloud config set compute/region us-west4
-gcloud config set compute/zone us-west4-a
+gcloud config set compute/region us-east1
+gcloud config set compute/zone us-east1-d
 
 # creat compute resource 
-gcloud compute instances create --machine-type=f1-micro nucleus-jumphost-217
+gcloud compute instances create --machine-type=f1-micro nucleus-jumphost-499
 
 #create compute clusters
 gcloud container clusters create --machine-type=n1-standard-1 --zone=us-west4-a kub-cluster 
@@ -17,4 +17,4 @@ gcloud container clusters get-credentials kub-cluster
 kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:2.0
 
 #create kub cluster port
-kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+kubectl expose deployment hello-server --type=LoadBalancer --port 8082
