a87fa69ec978b92bfe751685e3f7a0b89c577609
KiTech <emmakagbabuwe@gmail.com>
2023-06-19 19:04:43 +0000

    initial commit

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#set compute region/zone
+gcloud config set compute/region us-west4
+gcloud config set compute/zone us-west4-a
+
+# creat compute resource 
+gcloud compute instances create --machine-type=f1-micro nucleus-jumphost-217
+
+#create compute clusters
+gcloud container clusters create --machine-type=n1-standard-1 --zone=us-west4-a kub-cluster 
+
+#authenticate
+gcloud container clusters get-credentials kub-cluster 
+
+#create deploy
+kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:2.0
+
+#create kub service
+kubectl expose deployment hello-server --type=LoadBalancer --port 8080
+
